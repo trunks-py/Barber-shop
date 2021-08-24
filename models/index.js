70f111b82d81fd490d7fbe6756dce83dcc679bad
jpoulin587 <jpoulin587a@gmail.com>
2021-08-24 13:16:57 -0500

    user and service models built

---
+++ models/index.js
@@ -0,0 +1,14 @@
+const User = require('./User');
+const Activity = require('./Activity');
+const Service = require('./Service');
+
+// User.hasMany(Article, {
+//   foreignKey: 'user_id',
+//   onDelete: 'CASCADE'
+// });
+
+// Article.belongsTo(User, {
+//   foreignKey: 'user_id'
+// });
+
+module.exports = { User, Activity. Service };
