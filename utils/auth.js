dc609b6f966b3e439f7f968fb75d6f1fd8a91a23
salvadordiaz1222 <salvador.diaz1222@gmail.com>
2021-08-24 19:17:21 -0500

    auth file ready to use

---
+++ utils/auth.js
@@ -0,0 +1,12 @@
+function withAuth(req, res, next) {
+  console.log("with auth");
+  const { logged_in } = req.session;
+  if (!logged_in) {
+    console.log("not logged in");
+    return res.redirect("/");
+  }
+  console.log("logged in");
+  next();
+}
+
+module.exports = withAuth;
