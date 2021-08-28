e871f6dcab918fc51dfef3fb2fd07449b92fece9
jpoulin587 <jpoulin587a@gmail.com>
2021-08-28 10:44:00 -0500

    got server running

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -48,3 +48,5 @@ router.post("/login", async (req, res) => {
     res.status(400).json(err);
   }
 });
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
-//const exphbs = require('express-handlebars');
-//const routes = require('./controllers');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
 //const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
@@ -27,14 +27,14 @@ const sess = {
 app.use(session(sess));
 
 // Inform Express.js on which template engine to use
-//app.engine('handlebars', hbs.engine);
-//app.set('view engine', 'handlebars');
+// app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-//app.use(routes);
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
