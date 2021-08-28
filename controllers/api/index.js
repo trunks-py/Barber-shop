e4ea132981f1efd3cc9193008d48ad98389cd1cb
salvadordiaz1222 <salvador.diaz1222@gmail.com>
2021-08-28 11:04:34 -0500

    added the service routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const userRoutes = require("./userRoutes");
+const serviceRoutes = require("./serviceRoutes");
 
 router.use("/users", userRoutes);
+router.use("/services", serviceRoutes);
 
 module.exports = router;
