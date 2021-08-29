ac0f589cad7b7e9a461c392978a29811e2d4189a
jpoulin587 <jpoulin587a@gmail.com>
2021-08-29 01:01:45 -0500

    create new users built, needs debugging

--- controllers/api/serviceRoutes.js
+++ controllers/api/serviceRoutes.js
@@ -1,5 +1,5 @@
-const router = require("express").Router();
-const { Service } = require("../../models");
+const router = require('express').Router();
+const { Service } = require('../../models');
 
 // router.get("/", async (req, res) => {
 //     try {
