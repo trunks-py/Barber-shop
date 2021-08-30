4e6f63c755f8bf6a37d3a7968ad88607bd79b810
salvadordiaz1222 <salvador.diaz1222@gmail.com>
2021-08-30 11:18:31 -0500

    added delete route

--- controllers/api/serviceRoutes.js
+++ controllers/api/serviceRoutes.js
@@ -2,8 +2,8 @@ const router = require("express").Router();
 const { Service } = require("../../models");
 
 router.post("/", async (req, res) => {
-  const { service_name, price } = req.body;
-  if (!service_name || !price)
+  const { serviceName, price } = req.body;
+  if (!serviceName || !price)
     throw new Error("You must provide a service name and a price");
   try {
     const newService = await Service.create({
@@ -24,4 +24,23 @@ router.get("/", async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const serviceData = await Service.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!serviceData) {
+      res.status(404).json({ message: "No service found with this id" });
+      return;
+    }
+
+    res.status(200).json(serviceData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
