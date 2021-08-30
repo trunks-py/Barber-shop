fbe8decebe20a2e2422282276037b27f2aa80fc4
jpoulin587 <jpoulin587a@gmail.com>
2021-08-30 00:55:43 -0500

    started activity route

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,9 +2,12 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const serviceRoutes = require('./serviceRoutes');
 const manageUserRoutes = require('./manageUserRoutes');
+const activityRoutes = require('./activityRoute')
+
 
 router.use('/users', userRoutes);
 router.use('/services', serviceRoutes);
 router.use('/manageUsers', manageUserRoutes);
+router.use('/activity' , activityRoutes);
 
 module.exports = router;
