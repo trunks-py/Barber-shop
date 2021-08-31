c502e4ce85526f1b582538331ef0a35c26f9ff41
jpoulin587 <jpoulin587a@gmail.com>
2021-08-30 21:05:55 -0500

    add new user fixed

--- public/js/login.js
+++ public/js/login.js
@@ -24,54 +24,8 @@ const loginFormHandler = async (event) => {
   }
 };
 
-const signupFormHandler = async (event) => {
-  event.preventDefault();
 
-  const firstName = document.querySelector('#first-name-signup').value.trim();
-  const lastName = document.querySelector('#last-name-signup').value.trim();
-  const email = document.querySelector('#email-signup').value.trim();
-  const password = document.querySelector('#password-signup').value.trim();
-
-  if (firstName && lastName && email && password) {
-    const response = await fetch('/api/manageUsers', {
-      method: 'POST',
-      body: JSON.stringify({ firstName, lastName, email, password }),
-      headers: { 'Content-Type': 'application/json' },
-    });
-
-    if (response.ok) {
-      document.location.replace('/manageUsers');
-    } else {
-      alert(response.statusText);
-    }
-  }
-};
-
-const delButtonHandler = async (event) => {
-  if (event.target.hasAttribute('data-id')) {
-    const id = event.target.getAttribute('data-id');
-
-    const response = await fetch(`/api/manageUsers/${id}`, {
-      method: 'DELETE',
-    });
-
-    if (response.ok) {
-      document.location.replace('/manageUsers');
-    } else {
-      alert('Failed to delete user');
-    }
-  }
-};
-
-
-document
-  .querySelector('.user-list')
-  .addEventListener('click', delButtonHandler);
 
 document
   .querySelector('.login-form')
   .addEventListener('submit', loginFormHandler);
-
-document
-  .querySelector('.signup-form')
-  .addEventListener('submit', signupFormHandler);
