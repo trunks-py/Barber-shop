beff5f5e6daf908b5ee6518186a82ae3a4a92595
salvadordiaz1222 <salvador.diaz1222@gmail.com>
2021-08-30 21:46:57 -0500

    commented out withAuth

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,9 @@
-const router = require('express').Router();
-const apiRoutes = require('./api');
-const homeRoutes = require('./homeRoutes');
+const router = require("express").Router();
+const apiRoutes = require("./api");
+const homeRoutes = require("./homeRoutes");
+//const withAuth = require("../utils/auth");
 
-
-router.use('/api', apiRoutes);
-router.use('/', homeRoutes);
-
+router.use("/api", apiRoutes);
+router.use("/", homeRoutes);
 
 module.exports = router;
