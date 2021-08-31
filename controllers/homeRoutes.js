459a2795f280d6c7e24854342e5b49667885810e
salvadordiaz1222 <salvador.diaz1222@gmail.com>
2021-08-30 21:45:22 -0500

    added a get route for services

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,13 +1,25 @@
 const router = require("express").Router();
-const { Project } = require("../models");
-const withAuth = require("../utils/auth");
+const { Project, Service } = require("../models");
 //const getProjectsByUser = require('../utils/helpers');
+
+router.get("/services", async (req, res) => {
+  try {
+    const dbServices = await Service.findAll();
+
+    const services = dbServices.map((service) => service.get({ plain: true }));
+
+    res.render("service", { services });
+
+    // res.json(dbServices);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // Prevent non logged in users from viewing the homepage
 router.get("/", async (req, res) => {
   try {
-  
-    res.render('homepage');
+    res.render("homepage");
     // res.status(200).json(projects);
   } catch (err) {
     res.status(500).json(err);
@@ -16,10 +28,9 @@ router.get("/", async (req, res) => {
 
 router.get("/login", (req, res) => {
   if (req.session.logged_in) {
-    return res.redirect('/');
+    return res.redirect("/");
   }
   res.render("login");
 });
-
 
 module.exports = router;
